eb96496ee21ea7e741fa67c485fb8598903ae4c4
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-01-20 16:38:58 +0530

    Create Profile Page Java Class

---
+++ src/main/java/com/qa/democart/pages/ProfilePage.java
@@ -0,0 +1,11 @@
+package com.qa.democart.pages;
+
+public class ProfilePage {
+	public ProfilePage() {
+		System.out.println("PP default const");
+	}
+
+	public void getTitle() {
+		System.out.println("Profile Page title");
+	}
+}
