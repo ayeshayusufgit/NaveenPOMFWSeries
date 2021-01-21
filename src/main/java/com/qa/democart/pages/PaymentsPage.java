96e90f2a70141d8abec6bd69e658d2333dce05f7
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-01-21 15:11:51 +0530

    Adding the By Locators and Page Actions in the Payments Page

--- src/main/java/com/qa/democart/pages/PaymentsPage.java
+++ src/main/java/com/qa/democart/pages/PaymentsPage.java
@@ -1,12 +1,20 @@
 package com.qa.democart.pages;
 
+import org.openqa.selenium.By;
+
 public class PaymentsPage {
-	
+
+	public By selectPaymentModeButton = By.id("payment");
+
 	public void getPaymentsPageTitle() {
 		System.out.println("Get PAyments Page title");
 	}
-	
+
 	public void selectPayment(String modeOfPayment) {
-		System.out.println("The mode of Payment"+modeOfPayment);
+		System.out.println("The mode of Payment" + modeOfPayment);
+	}
+	
+	public void placeOrder() {
+		System.out.println("Order is placed!!!");
 	}
 }
