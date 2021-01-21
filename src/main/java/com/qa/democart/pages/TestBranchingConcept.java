e44c633569f9e14907f3ad2babc8ca781c21159e
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-01-21 19:52:26 +0530

    Adding locators in the TestBranchingConcept class

--- src/main/java/com/qa/democart/pages/TestBranchingConcept.java
+++ src/main/java/com/qa/democart/pages/TestBranchingConcept.java
@@ -1,6 +1,9 @@
 package com.qa.democart.pages;
 
+import org.openqa.selenium.By;
+
 public class TestBranchingConcept {
+	public By locato=By.id("locator");
 
 	public void getPageTitle() {
 		System.out.println("Testing Branching Concept is the page title");
