088ef9b64bebb7a747d4dd56e2073856dddbaa87
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-01-21 14:26:49 +0530

    create PaymentsPage.java

---
+++ src/main/java/com/qa/democart/pages/PaymentsPage.java
@@ -0,0 +1,12 @@
+package com.qa.democart.pages;
+
+public class PaymentsPage {
+	
+	public void getPaymentsPageTitle() {
+		System.out.println("Get PAyments Page title");
+	}
+	
+	public void selectPayment(String modeOfPayment) {
+		System.out.println("The mode of Payment"+modeOfPayment);
+	}
+}
