9bd13bd96ea9c5d0d54cc3dcb51dfaec4c901b13
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-01-24 20:07:34 +0530

    Reverting the constant file back to original

--- src/main/java/com/qa/democart/utils/Constants.java
+++ src/main/java/com/qa/democart/utils/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
 	public static final String EMAIL_ADDRESS_NOT_FOUND_ERROR_MESSAGE_COUNT = "Warning: The E-Mail Address was not found in our records, please try again!";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
-	//public static final int ACCOUNTS_SECTION_COUNT = 4;
-	public static final int ACCOUNTS_SECTION_COUNT = 5;
+	public static final int ACCOUNTS_SECTION_COUNT = 4;
+	//public static final int ACCOUNTS_SECTION_COUNT = 5;
 	public static final String ACCOUNTS_CREATION_SUCCESS_MESSAGE="Your Account Has Been Created!";
 	public static final String REGISTER_SHEET_NAME="Register";
 	public static final String SEARCH_SHEET_NAME="Search";
