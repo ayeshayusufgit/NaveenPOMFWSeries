4506e22376022afc23d698bb4fb073eb7fe492cf
ayeshayusufgit <ayesha.yusuf@gmail.com>
2021-02-23 20:22:20 +0530

    Adding the wishlist feature

---
+++ src/main/java/com/qa/democart/pages/WishlistPage.java
@@ -0,0 +1,16 @@
+package com.qa.democart.pages;
+
+import org.openqa.selenium.By;
+
+public class WishlistPage {
+	
+	
+	By addToWishlist = By.id("wishListIcon");
+	
+	
+	
+	public void addToWishlist() {
+	System.out.println("Adding to the wishlist");	
+	}
+
+}
